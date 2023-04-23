c3020651046536396095a23377702ae3bebd9134
Gabriel Cassimiro <gabriel.soares@matrizit.com.br>
2023-04-23 03:15:30 -0300

    CMD-1 - microservice of products has been structured and basic operations has implated CMD-1 - Created mvc for consuming a microservice of products

---
+++ RestWithASPNETUdemy/GeekShoping/GeepShopping.ProductAPI/Config/ProductProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using GeepShopping.ProductAPI.Data.ValueObjects;
+using GeepShopping.ProductAPI.Model;
+
+namespace GeepShopping.ProductAPI.Config
+{
+    public class ProductProfile
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDTO, Product>();
+                config.CreateMap<Product, ProductDTO>();
+            });
+            return mappingConfig;
+        }
+    }
+}
---
+++ RestWithASPNETUdemy/GeekShoping/GeepShopping.ProductAPI/Repository/ProductRepository.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using GeepShopping.ProductAPI.Data.ValueObjects;
+using GeepShopping.ProductAPI.Model;
+using GeepShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeepShopping.ProductAPI.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private IProductContext _dbContext;
+        private IMapper _mapper;
+
+        public ProductRepository(IProductContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<ProductDTO> Create(ProductDTO dto)
+        {
+            var product = _mapper.Map<Product>(dto);
+            _dbContext.Products.Add(product);
+            await _dbContext.SaveChangesAsync();
+            return _mapper.Map<ProductDTO>(product);
+        }
+
+        public async Task<bool> Delete(long id)
+        {
+            try
+            {
+                var products = await _dbContext.Products.FirstOrDefaultAsync(p => p.Id == id);
+                if(products == null)
+                    return false;
+                _dbContext.Products.Remove(products);
+                await _dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public async Task<IEnumerable<ProductDTO>> FindAll()
+        {
+            var products = await _dbContext.Products.ToListAsync();
+            return _mapper.Map<List<ProductDTO>>(products);
+        }
+
+        public async Task<ProductDTO> FindById(long id)
+        {
+            var products = await _dbContext.Products.FirstOrDefaultAsync(p => p.Id == id);
+            return _mapper.Map<ProductDTO>(products);
+        }
+
+        public async Task<ProductDTO> Update(ProductDTO dto)
+        {
+            var product = _mapper.Map<Product>(dto);
+            _dbContext.Products.Update(product);
+            await _dbContext.SaveChangesAsync();
+            return _mapper.Map<ProductDTO>(product);
+        }
+    }
+}
